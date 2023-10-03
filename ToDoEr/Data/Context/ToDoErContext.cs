4829d249610a58bca9d2c47e810c486705d83863
vladbuzan <vladbuzan98@gmail.com>
2023-10-03 22:39:41 +0300

    add first entity

--- ToDoEr/Data/Context/ToDoErContext.cs
+++ ToDoEr/Data/Context/ToDoErContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Context;
 
 public class ToDoErContext : DbContext
 {
     public ToDoErContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<ToDo> ToDos { get; set; } = null!;
 }
