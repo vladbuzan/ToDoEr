5fbf2aaa2e711db96df77f2488017e85b0450bb0
vladbuzan <vladbuzan98@gmail.com>
2023-10-03 22:30:04 +0300

    add dotnet tools manifest

--- ToDoEr/API/Program.cs
+++ ToDoEr/API/Program.cs
@@ -1,10 +1,14 @@
-﻿using Infrastructure.Extensions;
+﻿using Infrastructure.Exceptions;
+using Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 builder.Services.AddControllers();
-builder.Services.AddToDoErDb(config, "Db");
+
+builder.Services.AddToDoErDb(config.GetConnectionString("Db") ??
+    throw new SetupException("Db connection string is not set"));
+
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
--- ToDoEr/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ ToDoEr/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,23 +1,15 @@
 ﻿using Data.Context;
-using Infrastructure.Exceptions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddToDoErDb(this IServiceCollection services,
-        ConfigurationManager configManager,
-        string name)
-    {
-        var connectionString = configManager.GetConnectionString(name);
-
-        if (string.IsNullOrEmpty(connectionString))
-            throw new SetupException("Connection string is not set");
-
-        return services
-            .AddDbContext<ToDoErContext>(options => options.UseNpgsql(connectionString));
-    }
+    public static IServiceCollection AddToDoErDb(
+        this IServiceCollection services,
+        string connectionString
+    ) => services
+        .AddDbContext<ToDoErContext>(options =>
+            options.UseNpgsql(connectionString));
 }
